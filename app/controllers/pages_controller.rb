6f546c9bd0940a46d88787fc22eb79d50866d0f4
Andrew <schneider8@yahoo.com>
2021-06-27 15:10:33 -0400

    trying to get github pages to work

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  def home
+  def index
     StockQuote::Stock.new(api_key: 'sk_971f54cd2c014652bb9520c8bf7e0e41')
     @owned_stocks = []
     @owned_stocks << StockQuote::Stock.quote("wish")
