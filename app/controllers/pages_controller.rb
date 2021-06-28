b5a27d67442bc6b5c995ebd1479a99b6cb126d51
Andrew <schneider8@yahoo.com>
2021-06-27 20:50:22 -0400

    added eth

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -18,20 +18,50 @@ class PagesController < ApplicationController
     @chart_data2 = []
     @index2 = 0
     while @chart_data2.length < @wish.length do
+      # @wish[@index2] = Time.at(@wish[@index2])
       @chart_data2 << [@wish[@index2], @wish2[@index2]]
       @index2 += 1
     end
-    @chosen_stock_data = 'DOGE'
+    @chosen_stock_data = 'ETH'
     @cp = Cryptocompare::HistoMinute.find(@chosen_stock_data, 'USD').fetch("Data")
     @timestamps = @cp.pluck("time")
     @points = @cp.pluck("open", "high", "low", "close")
     @chart_data = []
     @index = 0
     while @chart_data.length < @timestamps.length do
+      @timestamps[@index] = Time.at(@timestamps[@index]).to_datetime
       @chart_data << [@timestamps[@index], @points[@index]]
       @index += 1
     end
-    
+  end
+
+  def crypto_chart(name)
+    @wish_data = StockQuote::Stock.chart(name)
+    @wish = @wish_data.chart.pluck("updated")
+    @wish2 = @wish_data.chart.pluck( "open", "high", "low", "close")
+    @chart_data2 = []
+    @index2 = 0
+    while @chart_data2.length < @wish.length do
+      # @wish[@index2] = Time.at(@wish[@index2])
+      @chart_data2 << [@wish[@index2], @wish2[@index2]]
+      @index2 += 1
+    end
+    render :partial => "chart", :locals => {name: name, data: @chart_data2}
+  end
+
+  def stock_chart(name)
+    @chosen_stock_data = name
+    @cp = Cryptocompare::HistoMinute.find(@chosen_stock_data, 'USD').fetch("Data")
+    @timestamps = @cp.pluck("time")
+    @points = @cp.pluck("open", "high", "low", "close")
+    @chart_data = []
+    @index = 0
+    while @chart_data.length < @timestamps.length do
+      @timestamps[@index] = Time.at(@timestamps[@index]).to_datetime
+      @chart_data << [@timestamps[@index], @points[@index]]
+      @index += 1
+    end
+    render :partial => "chart", :locals => {name: name, data: @chart_data}
   end
  
   private
